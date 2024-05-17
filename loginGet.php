5af513c08bf7ca29c9a3ed8059cec93eab7021d7
RafalyAhmad <136140993+RafalyAhmad@users.noreply.github.com>
2024-05-17 22:22:22 +0700

    LoginGet

---
+++ loginGet.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Selamat Datang</title>
+</head>
+<body>
+    <h1>Selamat datang <?php echo $_POST["nama"]; ?></h1>
+
+    <a href="get.php"> Logout
+</body>
+</html>
